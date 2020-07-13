f5d8cbbfdc2a442e8ac0a4dcb364133166e791ed
jitender85 <jitenderwebdesigner@gmail.com>
2020-07-13 20:26:27 +0530

    Updated react demo apps.

---
+++ tuts/reactjs/demo/react-with-hooks/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+import ClassCounter from './components/ClassCounter'
+import HookCounter from './components/HookCounter'
+import HookCounterTwo from './components/HookCounterTwo'
+import ClassCounterTwo from './components/ClassCounterTwo'
+// import HookCounterThree from './components/HookCounterThree'
+// import HookCounterFour from './components/HookCounterFour'
+
+function App() {
+  return (
+    <div className="App">
+        <ClassCounter />
+        <HookCounter />
+				<ClassCounterTwo />
+				<HookCounterTwo />
+				{/* <HookCounterThree />
+				<HookCounterFour /> */}
+			</div>
+  );
+}
+
+export default App;
